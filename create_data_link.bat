34d1e58dbd3ca027731ce441c00a802d3fe57814
Alexander Chernyshev <alexander.v.chernyshev@mail.ru>
2020-09-27 18:00:40 +0300

    baseline code fixed

---
+++ create_data_link.bat
@@ -0,0 +1,2 @@
+mklink /D "C:\Users\Alexander\Documents\GitHub\Kaggle\KaggleOSICPulmonaryFibrosisProgression\data" "F:\Projects data, datasets\Competitions\[2020.09] OSIC Pulmonary Fibrosis Progression\data"
+pause
